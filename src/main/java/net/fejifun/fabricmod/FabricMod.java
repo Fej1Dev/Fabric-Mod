18764f40770e6fb5aa830da35099c1e97bfeba62
Fej1Dev <101066376+Fej1Dev@users.noreply.github.com>
2023-01-25 20:45:10 +0200

    Initial commit

---
+++ src/main/java/net/fejifun/fabricmod/FabricMod.java
@@ -0,0 +1,10 @@
+package net.fejifun.fabricmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
